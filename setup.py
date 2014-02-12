544905636f64ef4742590fa606d7ec1cee54b599
Tim Behrsin <tim@behrsin.com>
2014-02-12 14:34:23 +0000

    updated nat logic to support turning off depending on nat=no in rr uri param

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
 	name = 'p2psip',
-	version = '2.2.6',
+	version = '2.2.7',
 	description = '39 Peers: Opensource peer-to-peer Internet telephony (P2P-SIP) software in Python',
 	author = 'Kundan Singh',
 	author_email = 'kundan10@gmail.com',
